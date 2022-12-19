893e8382f508ac4f7c455edc24291b365d0e0101
aaron <josh@joshs-mbp.attlocal.net>
2022-12-18 20:35:19 -0800

    completed Select component

--- src/components/ProgressBar/ProgressBar.js
+++ src/components/ProgressBar/ProgressBar.js
@@ -5,82 +5,154 @@ import styled from 'styled-components';
 import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
+// https://developer.mozilla.org/en-US/docs/Web/Accessibility/ARIA/Roles/progressbar_role
+
 // The read-only `aria-valuenow` should be provided and updated
 
 // use `aria-labelledby` if a visible label is present or `aria-label` if a visible label is not present.
 
 // If the progress bar is describing the loading progress of a particular region of a page, include the "aria-describedby" attribute to reference the progress bar's status, and set the "aria-busy" attribute to `true` on the region until it is finished loading.
+// ! mine
+// const Wrapper = styled.div`
+//   width: 370px;
+//   height: var(--height);
+//   background-color: ${COLORS.transparentGray15};
+//   border-radius: var(--border-radius);
+//   position: relative;
+//   box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+// `;
 
-const Wrapper = styled.div`
-  width: 370px;
-  height: var(--height);
-  background-color: ${COLORS.transparentGray15};
-  border-radius: var(--border-radius);
-  position: relative;
-  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
-`;
+// const DefaultBar = styled.div`
+//   width: 100%;
+//   height: 100%;
+//   position: absolute;
+//   top: 0px;
+//   bottom: 0px;
+//   left: 0px;
+//   right: 0px;
+//   margin: auto;
+//   border-radius: 4px;
+//   overflow: hidden;
+// `;
 
-const DefaultBar = styled.div`
-  width: 100%;
-  height: 100%;
-  position: absolute;
-  top: 0px;
-  bottom: 0px;
-  left: 0px;
-  right: 0px;
-  margin: auto;
-  border-radius: 4px;
-  overflow: hidden;
-`;
+// const LargeBar = styled(DefaultBar)`
+//   width: calc(100% - 8px);
+//   height: 16px;
+// `;
 
-const LargeBar = styled(DefaultBar)`
-  width: calc(100% - 8px);
-  height: 16px;
-`;
+// const LoadingProgress = styled.div`
+//   width: ${p => p.value + '%'};
+//   height: 100%;
+//   background-color: ${COLORS.primary};
+// `;
 
-const LoadingProgress = styled.div`
-  width: ${p => p.value + '%'};
-  height: 100%;
-  background-color: ${COLORS.primary};
-`;
+// const SIZES = {
+//   large: {
+//     '--height': '24px',
+//     '--border-radius': '8px',
+//   },
+//   medium: {
+//     '--height': '12px',
+//     '--border-radius': '4px',
+//   }, 
+//   small: {
+//     '--height': '8px',
+//     '--border-radius': '4px',
+//   },
+// };
 
-const SIZES = {
-  large: {
-    '--height': '24px',
-    '--border-radius': '8px',
+// const ProgressBar = ({ value, size }) => {
+//   const styles = SIZES[size];
+//   let Component;
+
+//   if (size === 'large') {
+//     Component = LargeBar;
+//   } else {
+//     Component = DefaultBar;
+//   }
+
+//   return (
+//     <Wrapper
+//       role="progressbar"
+//       aria-label="loading"
+//       aria-valuenow={value}
+//       style={styles}
+//     >
+//       <Component>
+//         <LoadingProgress value={value}></LoadingProgress>
+//       </Component>
+//     </Wrapper>
+//   );
+// };
+
+// ==========================
+// ! his
+const STYLES = {
+  small: {
+    height: 8,
+    padding: 0,
+    radius: 4,
   },
   medium: {
-    '--height': '12px',
-    '--border-radius': '4px',
-  }, 
-  small: {
-    '--height': '8px',
-    '--border-radius': '4px',
+    height: 12,
+    padding: 0,
+    radius: 4,
+  },
+  large: {
+    height: 16,
+    padding: 4,
+    radius: 8,
   },
 };
 
 const ProgressBar = ({ value, size }) => {
-  const styles = SIZES[size];
-  let Component;
+  const styles = STYLES[size];
 
-  if (size === 'large') {
-    Component = LargeBar;
-  } else {
-    Component = DefaultBar;
+  if (!styles) {
+    throw new Error(`Unknown size passed to ProgressBar: ${size}`);
   }
 
   return (
     <Wrapper
       role="progressbar"
-      aria-label="loading"
       aria-valuenow={value}
-      style={styles}
+      aria-valuemin="0"
+      aria-valuemax="100"
+      style={{
+        '--padding': styles.padding + 'px',
+        '--radius': styles.radius + 'px',
+      }}
     >
-      <Component>
-        <LoadingProgress value={value}></LoadingProgress>
-      </Component>
+      <VisuallyHidden>{value}%</VisuallyHidden>
+      <BarWrapper>
+        <Bar
+          style={{
+            '--width': value + '%',
+            '--height': styles.height + 'px',
+          }}
+        />
+      </BarWrapper>
     </Wrapper>
-  );
+  )
 };
+
+const Wrapper = styled.div`
+  background-color: ${COLORS.transparentGray15};
+  box-shadow: inset 0px 2px 4px ${COLORS.transparentGray35};
+  border-radius: var(--radius);
+  padding: var(--padding);
+`;
+
+const BarWrapper = styled.div`
+  border-radius: 4px;
+  overflow: hidden;
+`;
+
+const Bar = styled.div`
+  width: var(--width);
+  height: var(--height);
+  background-color: ${COLORS.primary};
+  border-radius: 4px 0 0 4px;
+`;
 
 export default ProgressBar;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -9,10 +9,57 @@ const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <Wrapper>
+      <DropDown value={value} onChange={onChange}>
+        {children}
+      </DropDown>
+      <SelectedText>{displayedValue}</SelectedText>
+      <DownArrow 
+        id={'chevron-down'}
+        size={24}
+        strokeWidth={2}
+      ></DownArrow>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div`
+  color: ${COLORS.gray700};
+  background-color: ${COLORS.transparentGray15};
+  height: 43px;
+  width: fit-content;
+  border-radius: 8px;
+  position: relative;
+  padding-right: 18px;
+
+  &:focus {
+    border: 2px solid ${COLORS.primary};
+  }
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const DropDown = styled.select`
+  position: absolute;
+  z-index: 1;
+  opacity: 0;
+  height: 100%;
+  width: 100%;
+`;
+
+const DownArrow = styled(Icon)`
+  position: relative;
+  top: 6px;
+  display: inline-block;
+`;
+
+const SelectedText = styled.p`
+  padding: 16px;
+  width: fit-content;
+  display: inline-block;
+  margin-top: -2px;
+`;
 
 export default Select;
