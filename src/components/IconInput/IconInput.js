d73529578023738d573d2b825da27983f8e46540
aaron <josh@joshs-mbp.attlocal.net>
2022-12-19 19:34:39 -0800

    completed IconInput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -6,14 +6,210 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import VisuallyHidden from '../VisuallyHidden';
 
+// MINE -- FAILED
+// const IconInput = ({
+//   label,
+//   icon,
+//   width = 250,
+//   size,
+//   placeholder,
+// }) => {
+//   return (
+//     <Wrapper style={{ '--width': width + 'px' }}>
+//       <IconWrapper>
+//         <TestIcon id={icon} strokeWidth={1} size={16} />
+//       </IconWrapper>
+//       <NativeInput placeholder={placeholder} type="text" />
+//     </Wrapper>
+//   )
+// };
+
+// const Wrapper = styled.div`
+//   width: var(--width);
+//   padding-left: 20px;
+//   border-bottom: 1px solid black;
+// `;
+
+// const NativeInput = styled.input`
+//   border: none;
+//   width: calc(100% + 19px);
+//   margin-left: -19px;
+//   padding-left: 24px;
+//   padding-right: 16px;
+
+//   :focus-visible {
+//     outline-offset: 4px;
+//   }
+// `;
+
+// const IconWrapper = styled.div`
+//   isolation: isolate;
+//   pointer-events: none;
+//   margin-left: -20px;
+//   margin-bottom: -15px;
+//   font-weight: 700;
+// `;
+
+// const TestIcon = styled(Icon)`
+//   font-weight: bold;
+// `;
+
+// ==================================================
+// OPTION 1
+// const STYLES = {
+//   small: {
+//     fontSize: 14,
+//     iconSize: 16,
+//     borderThickness: 1,
+//     height: 24,
+//   },
+//   large: {
+//     fontSize: 18,
+//     iconSize: 24,
+//     borderThickness: 2,
+//     height: 36,
+//   },
+// };
+
+// const IconInput = ({
+//   label,
+//   icon,
+//   width = 250,
+//   size,
+//   ...delegated
+// }) => {
+//   const styles = STYLES[size];
+
+//   return (
+//     <Wrapper>
+//       <VisuallyHidden>{label}</VisuallyHidden>
+//       <IconWrapper style={{ '--size': styles.iconSize + 'px' }}>
+//         <Icon id={icon} size={styles.iconSize}></Icon>
+//       </IconWrapper>
+//       <TextInput
+//         {...delegated}
+//         style={{
+//           '--width': width + 'px',
+//           '--height': styles.height / 16 + 'rem',
+//           '--border-thickness': styles.borderThickness + 'px',
+//           '--font-size': styles.fontSize / 16 + 'rem',
+//         }}
+//       />
+//     </Wrapper>
+//   )
+// };
+
+// const Wrapper = styled.label`
+//   display: block;
+//   position: relative;
+//   color: ${COLORS.gray700};
+
+//   &:hover {
+//     color: ${COLORS.black};
+//   }
+// `;
+
+// const IconWrapper = styled.div`
+//   position: absolute;
+//   top: 0;
+//   bottom: 0;
+//   margin: auto 0;
+//   height: var(--size);
+// `;
+
+// const TextInput = styled.input`
+//   width: var(--width);
+//   height: var(--height);
+//   font-size: var(--font-size);
+//   border: none;
+//   border-bottom: var(--border-thickness) solid ${COLORS.black};
+//   padding-left: var(--height);
+//   color: inherit;
+//   font-weight: 700;
+//   outline-offset: 2px;
+
+//   &::placeholder {
+//     font-weight: 400;
+//     color: ${COLORS.gray500};
+//   }
+// `;
+
+// ==================================================
+// OPTION 2 with composition.
+
 const IconInput = ({
   label,
   icon,
   width = 250,
   size,
-  placeholder,
+  ...delegated
 }) => {
-  return 'TODO';
+  const iconSize = size === 'small' ? 16 : 24;
+  const Input = size === 'small'
+    ? SmallInput
+    : LargeInput;
+
+    return (
+      <Wrapper>
+        <VisuallyHidden>{label}</VisuallyHidden>
+        <IconWrapper style={{ '--size': iconSize + 'px' }}>
+          <Icon id={icon} size={iconSize} />
+        </IconWrapper>
+        <Input
+          {...delegated}
+          style={{
+            '--width': width + 'px',
+          }}
+        />
+      </Wrapper>
+    );
 };
+
+const Wrapper = styled.label`
+  display: block;
+  position: relative;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: ${COLORS.black};
+  }
+`;
+
+const IconWrapper = styled.div`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  margin: auto 0;
+  height: var(--size);
+`;
+
+
+const TextInput = styled.input`
+  width: var(--width);
+  border: none;
+  color: inherit;
+  font-weight: 700;
+  outline-offset: 2px;
+
+  &::placeholder {
+    font-weight: 400;
+    color: ${COLORS.gray500};
+  }
+`;
+
+const SmallInput = styled(TextInput)`
+  height: 24px;
+  font-size: ${14 / 16}rem;
+  border-bottom: 1px solid ${COLORS.black};
+  padding-left: 24px;
+`;
+
+const LargeInput = styled(TextInput)`
+  height: 36px;
+  font-size: ${18 / 16}rem;
+  border-bottom: 2px solid ${COLORS.black};
+  padding-left: 36px;
+`;
+
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -5,61 +5,131 @@ import { COLORS } from '../../constants';
 import Icon from '../Icon';
 import { getDisplayedValue } from './Select.helpers';
 
+// ! MINE
+// const Select = ({ label, value, onChange, children }) => {
+//   const displayedValue = getDisplayedValue(value, children);
+
+//   return (
+//     <Wrapper>
+//       <DropDown value={value} onChange={onChange}>
+//         {children}
+//       </DropDown>
+//       <SelectedText>{displayedValue}</SelectedText>
+//       <DownArrow 
+//         id={'chevron-down'}
+//         size={24}
+//         strokeWidth={2}
+//       ></DownArrow>
+//     </Wrapper>
+//   );
+// };
+
+// const Wrapper = styled.div`
+//   color: ${COLORS.gray700};
+//   background-color: ${COLORS.transparentGray15};
+//   height: 43px;
+//   width: fit-content;
+//   border-radius: 8px;
+//   position: relative;
+//   padding-right: 18px;
+
+//   &:focus {
+//     border: 2px solid ${COLORS.primary};
+//   }
+
+//   &:hover {
+//     color: ${COLORS.black};
+//   }
+// `;
+
+// const DropDown = styled.select`
+//   position: absolute;
+//   z-index: 1;
+//   opacity: 0;
+//   height: 100%;
+//   width: 100%;
+// `;
+
+// const DownArrow = styled(Icon)`
+//   position: relative;
+//   top: 6px;
+//   display: inline-block;
+// `;
+
+// const SelectedText = styled.p`
+//   padding: 16px;
+//   width: fit-content;
+//   display: inline-block;
+//   margin-top: -2px;
+// `;
+
+// =================================
+// ! HIS
+
 const Select = ({ label, value, onChange, children }) => {
-  const displayedValue = getDisplayedValue(value, children);
+  const displayedValue = getDisplayedValue(value, children)
 
   return (
     <Wrapper>
-      <DropDown value={value} onChange={onChange}>
-        {children}
-      </DropDown>
-      <SelectedText>{displayedValue}</SelectedText>
-      <DownArrow 
-        id={'chevron-down'}
-        size={24}
-        strokeWidth={2}
-      ></DownArrow>
+      <NativeSelect value={value} onChange={onChange}>
+        {children} 
+      </NativeSelect>
+      <PresentationalBit>
+        {displayedValue}
+        <IconWrapper style={{ '--size': 24 + 'px' }}>
+          <Icon id="chevron-down" strokeWidth={1} size={24} />
+        </IconWrapper>
+      </PresentationalBit>
     </Wrapper>
   );
 };
 
 const Wrapper = styled.div`
+  position: relative;
+  width: max-content;
+`;
+
+const NativeSelect = styled.select`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  opacity: 0;
+  /* Allow the select to span the full height in Safari */
+  -webkit-appearance: none;
+`;
+
+const PresentationalBit = styled.div`
   color: ${COLORS.gray700};
   background-color: ${COLORS.transparentGray15};
-  height: 43px;
-  width: fit-content;
+  font-size: ${16 / 16}rem;
+  padding: 12px 16px;
+  padding-right: 52px;
   border-radius: 8px;
-  position: relative;
-  padding-right: 18px;
 
-  &:focus {
-    border: 2px solid ${COLORS.primary};
+  ${NativeSelect}:focus + & {
+    outline: 1px dotted #212212;
+    outline: 5px auto -webkit-focus-ring-color;
   }
 
-  &:hover {
+  ${NativeSelect}:hover + & {
     color: ${COLORS.black};
   }
 `;
 
-const DropDown = styled.select`
+const IconWrapper = styled.div`
   position: absolute;
-  z-index: 1;
-  opacity: 0;
-  height: 100%;
-  width: 100%;
-`;
-
-const DownArrow = styled(Icon)`
-  position: relative;
-  top: 6px;
-  display: inline-block;
-`;
-
-const SelectedText = styled.p`
-  padding: 16px;
-  width: fit-content;
-  display: inline-block;
-  margin-top: -2px;
+  top: 0;
+  bottom: 0;
+  right: 10px;
+  margin: auto;
+  width: var(--size);
+  height: var(--size);
+  // Using "pointer-events: none" handles the issue of
+  // clicking what is "behind" the element without dealing
+  // with z-index.
+  pointer-events: none;
 `;
 
 export default Select;
